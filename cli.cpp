15bf7d720bcd440a126b7dcfc01c5c280a00bf8d
Andrei N. Onea <andrei@onea.ro>
2024-03-18 23:16:47 +0200

    initial commit

---
+++ cli.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+#include "tc.h"
+
+int main()
+{
+	std::cout << add (2, 2) << '\n';
+	return 0;
+}
---
+++ tc.h
@@ -0,0 +1,6 @@
+#ifndef UPB_TC_DUMMY_H_
+#define UPB_TC_DUMMY_H_
+
+int add (int a, int b);
+
+#endif // "UPB_TC_DUMMY_H_"
