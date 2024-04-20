06d8103754d5b35fc4b29ca5102722ab8060f224
Andrei N. Onea <andrei@onea.ro>
2024-04-20 13:29:54 +0300

    bvc: empty structure

---
+++ BulkVolume.cpp
@@ -0,0 +1,13 @@
+#include "BulkVolume.h"
+#include "Utils.h"
+
+KTC_Result
+KTC_Bvc::classify(
+	const KTC_Data &trades,
+	FP_TYPE window,
+	KTC_BvcWindowType window_type,
+	KTC_BvcDegreesOfFreedom dof,
+	FP_TYPE start)
+{
+	return KTC_Result {};
+}
